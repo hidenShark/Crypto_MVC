c54fd0e0d1af170ffdb8614c26df8ad4cd26ff20
hidenShark <ilushashark@gmail.com>
2024-02-26 02:11:04 +0300

    Implementing logic in ViewController

--- Crypto_MVC/Crypto_MVC/Controllers/ViewController.swift
+++ Crypto_MVC/Crypto_MVC/Controllers/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tickerLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    private var count = 0
+    private var testData: [Crypto] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let model = Crypto.testData
+        setupData(with: model)
+        setupInitialState()
     }
 
+    // MARK: - Action
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        displayData(i: getRandomount())
+    }
+    
+    // MARK: - Method
 
+    func setupInitialState() {
+        displayData(i: count)
+    }
+    
+    func setupData(with testData: [Crypto]) {
+        self.testData = testData
+    }
+    
+    func displayData(i: Int) {
+        if testData.count >= 0 && count <= (testData.count - 1) && count >= 0 {
+            nameLabel.text = testData[i].name
+            tickerLabel.text = testData[i].ticker
+            valueLabel.text = String(testData[i].value)
+        } else {
+            print("Sorry, no data")
+        }
+    }
+    
+    func getRandomount() -> Int {
+        let randomCount = Int.random(in: 0 ..< testData.count)
+        return randomCount
+    }
 }
 
